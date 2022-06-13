d5e9d97609e67250b4dbaa240f2b1d91f94cf7e5
Gokul Das K <dgkgokul15@gmail.com>
2022-06-13 23:16:16 +0530

    text encryption with no key

--- lib/message_encrpt.dart
+++ lib/message_encrpt.dart
@@ -1,10 +1,11 @@
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:ui/home.dart';
 
 class txtencrypt extends StatefulWidget {
-  const txtencrypt({Key? key}) : super(key: key);
+  txtencrypt({Key? key}) : super(key: key);
 
   @override
   State<txtencrypt> createState() => _txtencryptState();
@@ -12,6 +13,24 @@ class txtencrypt extends StatefulWidget {
 
 class _txtencryptState extends State<txtencrypt> {
   @override
+  final _textcontrol = TextEditingController();
+  String data = "intial data";
+
+  String encryptedtext = "__________";
+  void encrypt_message() async {
+    final key = encrypt.Key.fromSecureRandom(32);
+    final iv = encrypt.IV.fromSecureRandom(16);
+    final Encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final encrypted = Encrypter.encrypt(data, iv: iv);
+    final decrypted = Encrypter.decrypt(encrypted, iv: iv);
+    print(decrypted);
+    print(encrypted.bytes);
+    print(encrypted.base16);
+    print(encrypted.base64);
+    encryptedtext = encrypted.bytes.toString();
+  }
+
+  void decrypt_mesage() {}
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Message or Text Encryption")),
@@ -21,6 +40,7 @@ class _txtencryptState extends State<txtencrypt> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               TextField(
+                controller: _textcontrol,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Enter the message to be encrypted ',
@@ -30,7 +50,7 @@ class _txtencryptState extends State<txtencrypt> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text(
+                  const Text(
                     "Enter the Key (optional) ",
                     textAlign: TextAlign.left,
                   ),
@@ -42,10 +62,6 @@ class _txtencryptState extends State<txtencrypt> {
                   hintText: 'Enter the key to encrpt',
                 ),
               ),
-              const Text(
-                "Encrytted code ",
-                textAlign: TextAlign.left,
-              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
@@ -56,7 +72,11 @@ class _txtencryptState extends State<txtencrypt> {
                       label: Text("Encrypt with key"),
                     ),
                     ElevatedButton.icon(
-                      onPressed: () {},
+                      onPressed: () {
+                        data = _textcontrol.text;
+                        print(data);
+                        encrypt_message();
+                      },
                       icon: Icon(Icons.enhanced_encryption_rounded),
                       label: Text(
                         "Encrypt no key",
@@ -64,6 +84,15 @@ class _txtencryptState extends State<txtencrypt> {
                     ),
                   ],
                 ),
+              ),
+              const Text(
+                "Encrytted code ",
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 10),
+              Text(
+                encryptedtext,
+                style: TextStyle(fontSize: 10),
               ),
               ElevatedButton(
                 onPressed: () {
