9f1a8e4fa913bf5d5706abe94c695ce0e86fe242
gokul153 <dgkgokul15@gmail.com>
2022-06-17 22:44:12 +0530

    encryptionmessagealpha

--- lib/message_encrpt.dart
+++ lib/message_encrpt.dart
@@ -22,6 +22,8 @@ class _txtencryptState extends State<txtencrypt> {
   int value = 0;
   int i = 0;
   String encryptedtext = "";
+  String decryptedtext = "";
+  var ciphervalue, dvalue;
   void encrypt_message(int key1) async {
     //final key = encrypt.Key.;
     //final iv = encrypt.IV.fromSecureRandom(16);
@@ -33,12 +35,14 @@ class _txtencryptState extends State<txtencrypt> {
     //print(encrypted.base16);
     //print(encrypted.base64);
     //encryptedtext = encrypted.bytes.toString();
-    print(data);
+    //print(data);
+    encryptedtext = "";
     for (i = 0; i < data.length; i++) {
       int value = data.codeUnitAt(i);
       print("orginal value ");
       print(value);
-      if (value + key >= 90) {
+      ciphervalue = value + key;
+      if (ciphervalue > 122) {
         value = value - 26 + key;
       } else {
         value = value + key;
@@ -46,11 +50,30 @@ class _txtencryptState extends State<txtencrypt> {
       encryptedtext += String.fromCharCode(value);
       //  String oneAsString = 1.toString();
       //print(value);
+      print("encrypted text");
       print(encryptedtext);
     }
   }
 
-  void decrypt_mesage() {}
+  void decrypt_mesage(int key1) async {
+    //decryptedtext = "";
+    for (i = 0; i < encryptedtext.length; i++) {
+      int dvalue = encryptedtext.codeUnitAt(i);
+      ciphervalue = dvalue - key;
+      print("dvalue");
+      print(dvalue);
+      if (ciphervalue < 97) {
+        dvalue = dvalue + 26 - key;
+      } else {
+        dvalue = dvalue - key;
+      }
+      print("$dvalue after decryption");
+      decryptedtext += String.fromCharCode(dvalue);
+    }
+    print("decryption completed");
+    print(decryptedtext);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text("Message or Text Encryption")),
@@ -63,7 +86,7 @@ class _txtencryptState extends State<txtencrypt> {
                 controller: _textcontrol,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
-                  hintText: 'Enter the message to be encrypted ',
+                  hintText: 'Enter the message to be encrypted in alphabets ',
                 ),
               ),
               Column(
@@ -84,17 +107,9 @@ class _txtencryptState extends State<txtencrypt> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(20.0),
                 child: Row(
                   children: [
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        // key = _keycontrol.text;
-                        //   data = _keycontrol.text;
-                      },
-                      icon: Icon(Icons.enhanced_encryption_rounded),
-                      label: Text("Encrypt with key"),
-                    ),
                     ElevatedButton.icon(
                       onPressed: () {
                         data = _textcontrol.text;
@@ -109,18 +124,76 @@ class _txtencryptState extends State<txtencrypt> {
                         "Encrypt no key",
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          decryptedtext = "";
+                          // key = _keycontrol.text;
+                          //   data = _keycontrol.text;
+                          setState(() {
+                            decrypt_mesage(key);
+                          });
+                        },
+                        icon: Icon(Icons.enhanced_encryption_rounded),
+                        label: Text("Decrypt"),
+                      ),
+                    ),
                   ],
                 ),
               ),
-              const Text(
-                "Encrytted code ",
-                textAlign: TextAlign.left,
-              ),
+
               SizedBox(height: 10),
               Text(
-                encryptedtext,
-                style: TextStyle(fontSize: 10),
+                "encypted message",
+                style: TextStyle(fontSize: 20),
               ),
+              Container(
+                width: 240.0,
+                height: 42.0,
+                decoration: BoxDecoration(
+                  // borderRadius: BorderRadius.circular(24.0),
+                  border: Border.symmetric(),
+                  color: const Color(0xff2c2c2c),
+                ),
+                child: SizedBox(
+                  child: Text(
+                    encryptedtext,
+                    style: TextStyle(
+                      fontFamily: 'Arial',
+                      fontSize: 20,
+                      color: Colors.white,
+                      height: 2,
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                "Decrypted message",
+                style: TextStyle(fontSize: 20),
+              ),
+              // SizedBox.fromSize(size: 10),
+              Container(
+                width: 240.0,
+                height: 42.0,
+                decoration: BoxDecoration(
+                  // borderRadius: BorderRadius.circular(24.0),
+                  border: Border.symmetric(),
+                  color: const Color(0xff2c2c2c),
+                ),
+                child: SizedBox(
+                  child: Text(
+                    decryptedtext,
+                    style: TextStyle(
+                      fontFamily: 'Arial',
+                      fontSize: 20,
+                      color: Colors.white,
+                      height: 2,
+                    ),
+                  ),
+                ),
+              ),
+
               ElevatedButton(
                 onPressed: () {
                   Navigator.of(context).push(
