57afc9391dcfbd2fd5bdf2ba0842b5df29a251a2
gokul153 <dgkgokul15@gmail.com>
2022-06-26 18:49:49 +0530

    file picker added

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,59 @@
+import 'dart:io' as io;
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:ui/message_encrpt.dart';
 
 //simport 'package:packageandnavigation/message_encrpt.dart'
-class Home_screen extends StatelessWidget {
+class Home_screen extends StatefulWidget {
   const Home_screen({Key? key}) : super(key: key);
+
+  @override
+  State<Home_screen> createState() => _Home_screenState();
+}
+
+class _Home_screenState extends State<Home_screen> {
+  bool loading = false;
+
+  Future<bool> _requestPermission(Permission permission) async {
+    if (await permission.isGranted) {
+      return true;
+    } else {
+      var result = await permission.request();
+      if (result == PermissionStatus.granted) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void _pickFile() async {
+    // opens storage to pick files and the picked file or files
+    // are assigned into result and if no file is chosen result is null.
+    // you can also toggle "allowMultiple" true or false depending on your need
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    // if no file is picked
+    if (result == null) return;
+
+    // we will log the name, size and path of the
+    // first picked file (if multiple are selected)
+    print(result.files.first.name);
+    print(result.files.first.size);
+    print(result.files.first.path);
+    final file = result.files.first;
+
+    _openFile(file);
+  }
+
+  void _openFile(PlatformFile file) {
+    OpenFile.open(file.path);
+    print("opening file");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +111,9 @@ class Home_screen extends StatelessWidget {
                         IconButton(
                           onPressed: () {
                             print("select file");
+                            print("Downloading Requested");
+                            _pickFile();
+                            //   Download_to();
                           },
                           icon: Icon(
                             Icons.file_open_rounded,
